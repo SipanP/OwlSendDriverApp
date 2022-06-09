ec3eb7c07dde36593ec21b2eab2bafb0776a4fe3
Sipan Petrosyan <sdp120@ic.ac.uk>
2022-06-10 00:53:56 +0100

    Fixed react native element dependencies

--- app/components/DeliveryInformationCard.js
+++ app/components/DeliveryInformationCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Card } from "@rneui/themed";
+import { Card } from "react-native-elements";
 
 const DeliveryInformationCard = ({ name, address, isPickup }) => {
   return (
