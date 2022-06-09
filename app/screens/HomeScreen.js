a79e68118944ce3551960a8a0d37b07013de365b
Sipan Petrosyan <sipanpetrosyan0@gmail.com>
2022-06-09 03:53:41 +0100

    Implemented google-places-autocomplete

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView
+      styles={{
+        height: "100%",
+      }}
+    >
+      <View
+        styles={{
+          padding: 5,
+        }}
+      >
         <Image
           style={{
             width: 100,
@@ -13,6 +22,29 @@ const HomeScreen = () => {
           }}
           source={{
             uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Shield_of_Imperial_College_London.svg/1200px-Shield_of_Imperial_College_London.svg.png",
+          }}
+        />
+        {/* <Text>test12</Text> */}
+        <GooglePlacesAutocomplete
+          styles={{
+            textInputContainer: {
+              backgroundColor: "grey",
+            },
+            textInput: {
+              height: 38,
+              color: "#5d5d5d",
+              fontSize: 16,
+            },
+            container: {
+              flex: 0,
+            },
+          }}
+          placeholder="Where from?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          query={{
+            key: "AIzaSyCE2Ct-iHuI_2nNALaRghtfpNBj1gPhfcY",
+            language: "en",
           }}
         />
       </View>
