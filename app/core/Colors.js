3ff0239f43ee6c500bfe83f60c22c2d5e708b455
Sipan Petrosyan <sdp120@ic.ac.uk>
2022-06-13 01:14:52 +0100

    Added new primary colour derivatives

--- app/core/Colors.js
+++ app/core/Colors.js
@@ -1,5 +1,7 @@
 export default {
   primary: "slateblue", //"#6A5ACD",
+  primarySeeThrough: "rgba(106, 90, 205, 0.8)",
+  darkPrimary: "rgb(55,44,125)", //"#372c7d"
   secondary: "firebrick", //"#DC2C2C",
   dark: "black",
 };
