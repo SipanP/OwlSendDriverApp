31eaaa8298a5eb61763a92df5a9998ab7221bb4f
Sipan Petrosyan <sdp120@ic.ac.uk>
2022-06-13 01:14:00 +0100

    Added go button component

---
+++ app/components/GoButton.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import Colors from "../core/Colors";
+import IonIcon from "react-native-vector-icons/Ionicons";
+
+const GoButton = () => {
+  return (
+    <TouchableOpacity>
+      <View style={styles.container}>
+        <Text style={styles.goText}>GO</Text>
+        <IonIcon
+          name="rocket-outline"
+          size={35}
+          color="white"
+          style={{ marginTop: 6 }}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default GoButton;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 125,
+    backgroundColor: Colors.primary,
+    borderRadius: 15,
+    borderColor: Colors.darkPrimary,
+    borderWidth: 2,
+    paddingLeft: 10,
+    flexDirection: "row",
+  },
+  goText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 40,
+    marginRight: 5,
+  },
+});
